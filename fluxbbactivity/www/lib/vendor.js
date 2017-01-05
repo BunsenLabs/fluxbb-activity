fda43084c472211101fb9721e561091a9b6aadec
Jens John <dev@2ion.de>
2017-01-05 20:54:35 +0100

    js:python backend returns time in epoch seconds, not milliseconds

--- fluxbbactivity/www/lib/vendor.js
+++ fluxbbactivity/www/lib/vendor.js
@@ -107,7 +107,7 @@ function trigger() {
   fetch("api/last-update").then((resp) => {
     if(resp.status===200) {
       resp.json().then((d) => {
-        let lu = parseInt(d.v, 0xA);
+        let lu = parseInt(d.v, 0xA)*1000;/*[s]*/
         let ct = (new Date).getTime();
         setTimeout(trigger, 1000 * 930 - (ct - lu));
         document.querySelector("button#last-update").innerHTML = `Last update: ${new Date(lu * 1000)}`;
