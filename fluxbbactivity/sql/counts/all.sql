136ba1e829f9af490ce01db026bfe0eecabfb5c0
Jens John <dev@2ion.de>
2017-02-25 20:12:20 +0100

    Pretend to change things

--- fluxbbactivity/sql/counts/all.sql
+++ fluxbbactivity/sql/counts/all.sql
@@ -1,20 +1,13 @@
-SELECT    'posts', SUM(num_posts)
-FROM      forums
+SELECT  'posts', SUM(num_posts) FROM forums
 UNION
-SELECT    'topics', SUM(num_topics)
-FROM      forums
+SELECT  'topics', SUM(num_topics) FROM forums
 UNION
-SELECT    'users', COUNT(*)
-  FROM    users
-  WHERE   group_id != 0 AND id > 1
+SELECT  'users', COUNT(*) FROM users WHERE group_id != 0 AND id > 1
 UNION
-SELECT    'pending_registrations', COUNT(*)
-  FROM    users
-  WHERE   group_id = 0 AND id > 1
+SELECT  'pending_registrations', COUNT(*) FROM users WHERE group_id = 0 AND id > 1
 UNION
-  SELECT    'bans', COUNT(*)
-  FROM bans
+SELECT  'bans', COUNT(*) FROM bans
 UNION
-  SELECT 'messages',COUNT(*) FROM messages
+SELECT  'messages',COUNT(*) FROM messages
 UNION
-  SELECT 'users_today',COUNT(*) FROM users where last_visit >= UNIX_TIMESTAMP(CURRENT_DATE());
+SELECT  'users_today',COUNT(*) FROM users where last_visit >= UNIX_TIMESTAMP(CURRENT_DATE());
