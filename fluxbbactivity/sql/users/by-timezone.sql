9eba9b2b1f1660ad97f756a316853ec14df5fa4a
Jens John <dev@2ion.de>
2017-03-17 00:14:30 +0100

    sql:users/by-timezone:use relative shares

--- fluxbbactivity/sql/users/by-timezone.sql
+++ fluxbbactivity/sql/users/by-timezone.sql
@@ -1,1 +1,1 @@
-select timezone,count(*) as count from users group by timezone order by timezone asc;
+select timezone,count(*)/(select count(*) from users)*100 as count from users group by timezone order by timezone asc;
