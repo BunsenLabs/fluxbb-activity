c4b1ab110d74f077680ddb38420dbba2374bd0d0
Jens John <dev@2ion.de>
2017-03-16 21:59:06 +0100

    sql:topics/top-views:fix inconsistency with decimal place rounding

--- fluxbbactivity/sql/topics/top-views.sql
+++ fluxbbactivity/sql/topics/top-views.sql
@@ -1,1 +1,1 @@
-select id,subject,num_views,truncate(num_views/(select sum(num_views) from topics)*100,1) from topics order by num_views desc limit 5;
+select id,subject,num_views,round(num_views/(select sum(num_views) from topics)*100,2) from topics order by num_views desc limit 5;
