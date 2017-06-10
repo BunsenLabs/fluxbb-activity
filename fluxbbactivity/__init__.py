c7b74f0e1837511d09131de88062e9948a34ec37
Jens John <dev@2ion.de>
2017-06-10 23:39:48 +0200

    Revert "service: Split off classes into separate files"
    This reverts commit 427fed4a2d87f497b3b4e55169cbf6a99433ec47.

--- fluxbbactivity/__init__.py
+++ fluxbbactivity/__init__.py
@@ -2,10 +2,17 @@
 
 from argparse import ArgumentParser
 from bottle import abort, route, run, static_file
+import MySQLdb
+import calendar
+import datetime
 import logging
 import os
+import pathlib
 import sys
-from fluxbbactivity.fetcher import Fetcher
+import threading
+import time
+import sqlite3
+import json
 
 APIVER = 0
 PUBLIC = {}
@@ -35,6 +42,84 @@ def parse_cmdline():
         except:
             raise BaseException("No SQL password supplied via command line or environment")
     return args
+
+class Journal:
+    def __init__(self, path):
+        self.path = path
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.path)
+        cur = self.conn.cursor()
+        cur.execute(""" select name from sqlite_master where type='table' and name='journal'; """)
+        if not cur.fetchone():
+            cur.execute(""" create table journal (date TEXT, apiversion INTEGER, query TEXT, value TEXT ); """)
+        self.conn.commit()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.conn.close()
+
+    def commit(self, query, value):
+        cur = self.conn.cursor()
+        cur.execute(""" INSERT INTO journal VALUES ( ?, ?, ?, ? ) """,
+                (str(datetime.datetime.utcnow().isoformat()), APIVER, query, value,))
+        self.conn.commit()
+
+    def history(self, key):
+        cur = self.conn.cursor()
+        cur.execute(""" SELECT date,value
+                        FROM journal
+                        WHERE query = ? AND apiversion = ?""",
+                (key, APIVER,));
+        rows = list(map(lambda v: [ v[0], json.loads(v[1]) ], cur.fetchall()))
+        self.conn.commit()
+        return rows
+
+    def __history_views(self):
+        pass
+
+class Fetcher(threading.Thread):
+    def __init__(self, cconf, queries, timeout, journalpath):
+        super().__init__()
+        self.cconf = cconf
+        self.queries = queries
+        self.timeout = timeout
+        self.journal = journalpath
+        self.public = {}
+
+    def run(self):
+        self.event = threading.Event()
+        self.update()
+        while not self.event.wait(timeout=self.timeout):
+            self.update()
+
+    def update(self):
+        logging.info("Running Fetcher.update()")
+        global PUBLIC
+        PUBLIC = self.query()
+        logging.info("Fetcher.update() finished.")
+
+    def query(self):
+        t = { "history": dict() }
+        with MySQLdb.connect(**self.cconf) as cur:
+            with Journal(self.journal) as jur:
+                for cat in self.queries:
+                    t[cat] = {}
+                    t["history"][cat] = {}
+                    for key in self.queries[cat]:
+                        query_key = "{}/{}".format(cat, key)
+                        logging.debug("Executing query {}".format(query_key))
+                        cur.execute(self.queries[cat][key])
+                        t[cat][key] = [ self.convtuple(tup) for tup in cur.fetchall() ]
+                        jur.commit(query_key, json.dumps(t[cat][key]))
+                        if query_key == "counts/all":
+                                t["history"][cat][key] = jur.history(query_key)
+            t["ts"] = { "last_update": calendar.timegm(time.gmtime(time.time())),
+                        "update_interval": self.timeout }
+        return t
+
+    def convtuple(self, tup):
+        return list(tup[:-1]) + [ float(tup[-1]) ]
 
 @route("/api/{}/<cat>/<key>".format(APIVER))
 def dataroute(cat, key):
