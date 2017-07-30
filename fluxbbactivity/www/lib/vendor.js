0012067c6e39a39c1b00507e6e79249911af6457
Jens John <dev@2ion.de>
2017-07-30 23:58:58 +0200

    js: fix wrong css key

--- fluxbbactivity/www/lib/vendor.js
+++ fluxbbactivity/www/lib/vendor.js
@@ -146,7 +146,7 @@ function counter_chart_ts(key) {
 
 function counter_chart_show_key(key) {
   let row = document.querySelector("#counter-chart-row");
-  row.style.style.display = 'initial';
+  row.style.display = 'initial';
   let canvas = document.querySelector("#counter-chart");
   let ts = counter_chart_ts(key);
   ts = counter_chart_flatten_ts(ts);
